0618e3252685485c428db17f2ddf29469a00bdd2
Bob Baylor <SkiBoyBob@gmail.com>
2019-10-02 15:40:55 -0700

    port cap860.py to python 3.7 and update the readme

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-There are two scripts in this repo: stream.py and cap860.py They both collect data from an SRS lock-in amplifier. 
- * stream does it in real-time using UDP over an ethernet connection. 
+There are two scripts in this repo: stream.py and cap860.py They both collect data from an SRS lock-in amplifier.
+ * stream does it in real-time using UDP over an ethernet connection.
  * cap860 uses the lock-in capture buffer before downloading the data to the host computer.
- 
+
 cap860.py is detailed after stream.py
 ## Which one should I use?
- * stream.py uses UDP which may drop packets (data) depending on network traffic so if missing data is a problem, 
+ * stream.py uses UDP which may drop packets (data) depending on network traffic so if missing data is a problem,
  you might want to use cap860.py
- * cap860.py transfers using TCP/IP so packets are re-transmitted as needed and no data is lost. However, 
- the cap860.py transfer hapens after the data collection is complete so you must wait until the end.
- 
+ * cap860.py transfers using TCP/IP so packets are re-transmitted as needed and no data is lost. However,
+ the cap860.py transfer hapens after the data collection is complete so you must wait until the capture ends.
+
 # stream.py
 ## What is this?
 This python script streams data from a Stanford Research Systems SR865, SR865A, or SR860 lock-in amplifier.
@@ -19,7 +19,7 @@ It uses a Phase Synchronous Detector (PSD) to extract the in-phase and quadratur
 ## How do I run this script?
 You need a few things:
  * A computer with python
-   * I've tested this with python 2.7
+   * I've tested this with python 3.7.3
    * you'll need to install vxi11 using pip, brew, or whatever installation tool you use.
    * The script parses command line arguments with docopt, so you'll need to install that, too.
  * an [SR865](http://www.thinksrs.com/products/SR865A.htm), SR865A or SR860.
@@ -27,21 +27,21 @@ You need a few things:
 
 ## Is that all?
 You also need to
- * open your comupter's firewall for incoming UDP on the streaming port. This script defaults to 1865 but any port that 
+ * open your comupter's firewall for incoming UDP on the streaming port. This script defaults to 1865 but any port that
  doesn't conflict with some other service will work.
  * set the IP address of the SR865 to be visible to your computer and not conflict with other devices on your network.
  * use telnet to verify that your computer can connect to the SR865.
    * Type *idn? in telnet and you should see the identification string.
    * Exit telnet: Hold the control key and press ] to get to the telnet prompt, then type quit
 
-Both of these things may require help from your local IT person. Once you've done these things, 
+Both of these things may require help from your local IT person. Once you've done these things,
 it's a good idea to edit the default settings in stream.py The settings start around line 66.
  * Change the default IP to match the setting in the SR865 Setup/Ethernet menu.
  * Make sure the UDP Port setting matches the opened port in your firewall.
 
 # cap860.py
 ## What is this?
-This python script configures the instrument to capture data internally and, when complete, download it to the host computer. 
+This python script configures the instrument to capture data internally and, when complete, download it to the host computer.
 ## How do I run this script?
 You need all the things mentioned for stream.py, above
 
--- cap860.py
+++ cap860.py
@@ -29,7 +29,8 @@
 
         python cap860.py -h        to see the list of options
         Default options can be changed by editing the useStr (below).
-        You will certainly need to change the IP address to match your SR865 and for compatability with your network.
+        You will certainly need to change the IP address to match your SR865 and for
+        compatability with your network.
 
 """
 import math
@@ -43,15 +44,15 @@ import time
 try:
     import vxi11            # required
 except ImportError:
-    print 'required python vxi11 library not found. Please install vxi11'
+    print('required python vxi11 library not found. Please install vxi11')
 
 try:
-    import docopt           # handy command line parser. useStr (below) defines the syntax and documents it at the same time.
+    import docopt       # useStr (below) defines the syntax and documents it at the same time.
 except ImportError:
-    print 'python docopt library not found. Please install docopt or remove the docopt code from test() and main'
+    print('python docopt library not found. Please install docopt or remove the docopt code from test() and main') #pylint: disable=C0301
 
 
-useStr = """
+USE_STR = """
  --Capture Data on an SR865 and save it to a file--
  Usage:
   cap860  [--address=<A>] [--count=<C>] [--debug] [--file=<F>] [--mode=<M>] [--silent] [--vars=<V>] [--wait=<W>]
@@ -70,73 +71,76 @@ useStr = """
     """
 
 
-def show_status(l='',r=''):
+def show_status(left_t='', right_t=''):
     """ Simple text status line that overwrites itself to prevent scrolling.
     """
-    print ' %-30s %48s\r'%(l[:30],r[:48]),
+    print(' %-30s %48s\r'%(left_t[:30], right_t[:48]), end=' ')
 
 
 # global that gets assigned to the vxi11 object to allow SIGINT to cleanup properly
-vx      = None
+the_vx_ifc = None        #pylint: disable=global-statement, invalid-name
 
 
 def cleanup():
     """ Stop the stream and close the socket and vxi11.
     """
-    # global vx
-    print "\n cleaning up...",
-    vx.close()
-    print 'connections closed\n'
+    # global the_vx_ifc
+    print("\n cleaning up...", end=' ')
+    the_vx_ifc.close()
+    print('connections closed\n')
 
 
 def open_interfaces(ipadd):
-    """ open a vxi11 instrument and assign it to the global vx
+    """ open a vxi11 instrument and assign it to the global the_vx_ifc
     """
-    global vx
-    print'opening VXI-11 at %s ...' % ipadd,
-    vx = vxi11.Instrument(ipadd)
-    print 'done'
+    global the_vx_ifc           #pylint: disable=W0603,C0103
+    print('opening VXI-11 at %s ...'%ipadd, end=' ')
+    the_vx_ifc = vxi11.Instrument(ipadd)
+    print('done')
 
 
 
-def dut_config(vx, sStrChans, iWaitCount):
+def dut_config(vx_handle, str_chans, i_wait_count):
     """ Setup the SR865 for streaming. Return the total expected sample count
     """
-    vx.write('CAPTURECFG %s'%sStrChans)    # the vars to captures
-    iCapLenK = math.ceil(len(sStrChans) * iWaitCount / 256.0)
-    vx.write('CAPTURELEN %d'%iCapLenK)   # in kB. dut rounds odd numbers up to next even
-    # print 'CAPTURELEN %d'%iCapLenK       # in kB. dut rounds odd number up
-    fRateMax = float(vx.ask('CAPTURERATEMAX?'))      # filters determine the max data rate
-    return fRateMax
+    vx_handle.write('CAPTURECFG %s'%str_chans)    # the vars to captures
+    i_cap_len_k = math.ceil(len(str_chans) * i_wait_count / 256.0)
+    vx_handle.write('CAPTURELEN %d'%i_cap_len_k)   # in kB. dut rounds odd numbers up to next even
+    # print 'CAPTURELEN %d'%i_cap_len_k       # in kB. dut rounds odd number up
+    f_rate_max = float(vx_handle.ask('CAPTURERATEMAX?'))      # filters determine the max data rate
+    return f_rate_max
 
 
-def capture_data(vx, s_mode, s_channels, i_wait_count, t_timeout, b_show_status):
-    t_start = time.clock()
-    vx.write('CAPTURESTART ONE, %s'%s_mode)
+def capture_data(vx_handle, s_mode, s_channels, i_wait_count, t_timeout, b_show_status): #pylint: disable=R0913
+    """ tell the SR865 to take data and wait until it completes
+    """
+    t_start = time.perf_counter()
+    vx_handle.write('CAPTURESTART ONE, %s'%s_mode)
     i_bytes_captured = 0
     i_last_cap_byte = 0
     t_last = t_start
     while i_bytes_captured < (i_wait_count * 4 * len(s_channels)):
-        i_bytes_captured = int(vx.ask('CAPTUREBYTES?'))
+        i_bytes_captured = int(vx_handle.ask('CAPTUREBYTES?'))
         if b_show_status:
-            show_status('dut has captured %4d of %4d samples'%(i_bytes_captured / (4 * len(s_channels)), i_wait_count))
+            show_status('dut has captured %4d of %4d samples'%
+                        (i_bytes_captured / (4 * len(s_channels)), i_wait_count))
         if (i_bytes_captured - i_last_cap_byte) == 0:
-            if (time.clock() - t_last) > t_timeout:
-                print '\n\n**** CAPTURE TIMEOUT! ****'
+            if (time.perf_counter() - t_last) > t_timeout:
+                print('\n\n**** CAPTURE TIMEOUT! ****')
                 if not i_bytes_captured:
-                    print '**** NO DATA CAPTURED - missing trigger? ****\n'
+                    print('**** NO DATA CAPTURED - missing trigger? ****\n')
                     sys.exit(-1)
                 break
         else:
-            t_last = time.clock()
+            t_last = time.perf_counter()
         i_last_cap_byte = i_bytes_captured
-    t_end = time.clock()
-    vx.write('CAPTURESTOP')
-    print 'capture took %.3f seconds. Retrieving data...'%(t_end-t_start)
+    t_end = time.perf_counter()
+    vx_handle.write('CAPTURESTOP')
+    print('capture took %.3f seconds. Retrieving data...'%(t_end-t_start))
     return i_bytes_captured
 
 
-def retrieve_data(vx, i_bytes_captured, i_wait_count, s_channels):
+def retrieve_data(vx_handle, i_bytes_captured, i_wait_count, s_channels):
     """ Use the binary transfer command over vx interface to retrieve the capture buffer.
         maximum block count for CAPTUREGET? is 64 so loop over blocks as needed to
         get all the desired data.
@@ -150,47 +154,47 @@ def retrieve_data(vx, i_bytes_captured, i_wait_count, s_channels):
     i_retries = 0
     while i_bytes_remaining > 0:
         i_block_cnt = min(64, int(math.ceil(i_bytes_remaining / 1024.0)))
-        vx.write('CAPTUREGET? %d, %d'%(i_block_offset, i_block_cnt))
-        buf = vx.read_raw()         # read whatever dut sends
-        if not len(buf):
-            print 'empty response from dut for block %d'%i_block_offset
+        vx_handle.write('CAPTUREGET? %d, %d'%(i_block_offset, i_block_cnt))
+        buf = vx_handle.read_raw()         # read whatever dut sends
+        if not buf:
+            print('empty response from dut for block %d'%i_block_offset)
             i_retries += 1
             if i_retries > 5:
-                print '\n\n**** TOO MANY RETRIES ATTEMPTING TO GET DATA! ****'
+                print('\n\n**** TOO MANY RETRIES ATTEMPTING TO GET DATA! ****')
                 if not i_block_offset:
-                    print '**** NO DATA RETUNED ****\n'
+                    print('**** NO DATA RETUNED ****\n')
                     sys.exit(-1)
 
-        # binary block CAPTUREGET returns #nccccxxxxxxx... with little-endian float x bytes see manual page 139
+        # binary block CAPTUREGET returns #nccccxxxxxxx...
+        #   with little-endian float x bytes see manual page 139
         # if b_show_debug:
-        if False:
-            print ' '.join(['%02X'%ord(x) for x in buf[:6]])
-            print str_blocks_hex(buf[6:262])
+        #   print(' '.join(['%02X'%ord(x) for x in buf[:6]]))
+        #   print(str_blocks_hex(buf[6:262]))
 
         raw_data = buf[2 + int(buf[1]):]
         i_bytes_to_convert = min(i_bytes_remaining, len(raw_data))
-        f_block_data = list(unpack_from('<%df'%(i_bytes_to_convert/4), raw_data))      # convert to floats
+        # convert to floats
+        f_block_data = list(unpack_from('<%df'%(i_bytes_to_convert/4), raw_data))
         # if b_show_debug:
-        if False:
-            print len(f_block_data), 'floats received'
-            print str_blocks_float(f_block_data)
+        #   print(len(f_block_data), 'floats received')
+        #   print(str_blocks_float(f_block_data))
         f_data += f_block_data
         i_block_offset += i_block_cnt
         i_bytes_remaining -= i_block_cnt * 1024
     return f_data
 
 
-def write_to_file(file_name, s_channels, f_data, open_mode = 'a'):
+def write_to_file(file_name, s_channels, f_data, open_mode='a'):
     """ Save ASCII data to a comma separated file. We could also have used the csv python module...
     """
     if f_data:
         show_status('writing %s ...'%file_name)
-        with open(file_name, open_mode) as fl:
+        with open(file_name, open_mode) as f_out:
             s_fmt = '%+12.6e,'*len(s_channels)
-            fl.write(''.join(['%s,'%str.upper(v) for v in s_channels])+'\n')
+            f_out.write(''.join(['%s,'%str.upper(v) for v in s_channels])+'\n')
             for i in range(0, len(f_data)/len(s_channels), len(s_channels)):
-                x = f_data[i:i+len(s_channels)]
-                fl.write(s_fmt%tuple(x)+'\n')
+                a_line = f_data[i:i+len(s_channels)]
+                f_out.write(s_fmt%tuple(a_line)+'\n')
             show_status('%s written'%file_name)
     else:
         show_status('no data! File not writtten!')
@@ -200,77 +204,84 @@ def str_blocks_hex(buf):
     """ Handy formatting for looking at the binary data. Returns a string with spaces
         and line-feeds to make it easy to find locations.
     """
-    return ' '.join(['%s%s%s%s%02X'%('' if i==0 or i%32 != 0 else '\n', # add a line-feed every 8 blocks
-                                     '' if i==0 or i%128 != 0 else '\n',# add an extra line-feed every 4 lines
-                                     '' if i%4 != 0 else ' ',           # add an extra space every 4 values
-                                     '' if i%16 != 0 else ' ',          # add another extra space every block of 4
-                                     ord(x)) for i, x in enumerate(buf)])
+    return ' '.join(['%s%s%s%s%02X'%(
+        '' if i == 0 or i%32 != 0 else '\n', # add a line-feed every 8 blocks
+        '' if i == 0 or i%128 != 0 else '\n',# add an extra line-feed every 4 lines
+        '' if i%4 != 0 else ' ',           # add an extra space every 4 values
+        '' if i%16 != 0 else ' ',          # add another extra space every block of 4
+        ord(x)) for i, x in enumerate(buf)])
 
 def str_blocks_float(buf):
     """ Handy formatting for looking at the float data. Returns a string with spaces
         and line-feeds to make it easy to find locations.
     """
-    return ' '.join(['%s%s%s%12.4e'%('' if i==0 or i%8 != 0 else '\n',  # add a line-feed every 8 values
-                                     '' if i==0 or i%32 != 0 else '\n', # add an extra line-feed every 4 lines
-                                     '' if i%4 != 0 else ' ',           # add an extra space every 4 values
-                                     x) for i, x in enumerate(buf)])
+    return ' '.join(['%s%s%s%12.4e'%(
+        '' if i == 0 or i%8 != 0 else '\n',  # add a line-feed every 8 values
+        '' if i == 0 or i%32 != 0 else '\n', # add an extra line-feed every 4 lines
+        '' if i%4 != 0 else ' ',           # add an extra space every 4 values
+        x) for i, x in enumerate(buf)])
 
 
 # socket seems to catch the KeyboardInterrupt exception if I don't grab it explicitly here
-def interrupt_handler(signum, frame):
+def interrupt_handler(signum, frame):  #pylint: disable=W0613
+    """ handle the user kb int
+    """
     cleanup()
-    sys.exit(-2)      # Terminate process here as catching the signal removes the close process behaviour of Ctrl-C
+    sys.exit(-2)  # Terminate process here as catching the signal
+                  # removes the close process behaviour of Ctrl-C
 
 
 signal.signal(signal.SIGINT, interrupt_handler)
 
 
 
-def enforce_choice( key, d, allowed):
+def enforce_choice(key, d_obj, allowed):
     """ Some CLI args have a specific list of allowed values.
         Make sure we got one of the allowed values and return it upper case.
     """
-    if not d[key].upper() in allowed:
-        print '%s option was %s. Must be one of'%(key, d[key]), ' '.join(allowed)
+    if not d_obj[key].upper() in allowed:
+        print('%s option was %s. Must be one of'%(key, d_obj[key]), ' '.join(allowed))
         sys.exit(-1)
-    return d[key].upper()
+    return d_obj[key].upper()
 
 
 
-# the main program -----------------------------------------------
-def test(opts):
-    # global vx
+def test(options):
+    """ the main program -----------------------------------------------
+    """
+    # global the_vx_ifc
 
     # group the docopt stuff to make it easier to remove, if desired
-    dut_add  = opts['--address']                # IP address of the SR86x
-    i_wait_count = int(opts['--count'])         # how many points to capture
-    f_name = opts['--file']                     # file to write, if file name provided
-    b_show_status = not opts['--silent']
-    b_show_debug = opts['--debug']
-    t_timeout = float(opts['--wait'])           # give up if dut takes more than 'wait' seconds between points
+    dut_add = options['--address']                # IP address of the SR86x
+    i_wait_count = int(options['--count'])         # how many points to capture
+    f_name = options['--file']                     # file to write, if file name provided
+    b_show_status = not options['--silent']
+    b_show_debug = options['--debug']
+    t_timeout = float(options['--wait'])           # give up if >'wait' seconds between points
 
-    s_channels = enforce_choice('--vars', opts, ['X','XY','RT','XYRT'])
-    s_mode = enforce_choice('--mode', opts, ['IMM','TRIG','SAMP',])
+    s_channels = enforce_choice('--vars', options, ['X', 'XY', 'RT', 'XYRT'])
+    s_mode = enforce_choice('--mode', options, ['IMM', 'TRIG', 'SAMP',])
 
     # --------------- setup the capture ---------------------------
-    open_interfaces(dut_add)                    # sets the vx global to our comm object
-    f_rate_max = dut_config(vx, s_channels, i_wait_count)
-    vx.write('CAPTURESTOP')                     # stop any current capture
+    open_interfaces(dut_add)                    # sets the the_vx_ifc global to our comm object
+    f_rate_max = dut_config(the_vx_ifc, s_channels, i_wait_count)
+    the_vx_ifc.write('CAPTURESTOP')                     # stop any current capture
 
     show_status('waiting for capture (at least %.1f seconds)...'%(i_wait_count/f_rate_max))
     if 'IMM' not in s_mode:
-        show_status( 'FYI: apply trigger to BNC')
+        show_status('FYI: apply trigger to BNC')
 
     # --------------- capture the data and retieve it from the dut ------------
-    i_bytes_captured = capture_data(vx, s_mode, s_channels, i_wait_count, t_timeout, b_show_status)
-    f_data = retrieve_data(vx, i_bytes_captured, i_wait_count, s_channels)
+    i_bytes_captured = capture_data(the_vx_ifc, s_mode, s_channels, \
+                                    i_wait_count, t_timeout, b_show_status)
+    f_data = retrieve_data(the_vx_ifc, i_bytes_captured, i_wait_count, s_channels)
 
     # ------------- display or write the data to a file -----------------------
     if b_show_debug and i_bytes_captured:
-        print 'first 16:'
-        print str_blocks_float(f_data[:16])
-        print '   ...\nlast 16:'
-        print str_blocks_float(f_data[-16:])
+        print('first 16:')
+        print(str_blocks_float(f_data[:16]))
+        print('   ...\nlast 16:')
+        print(str_blocks_float(f_data[-16:]))
 
     if f_name is not None:
         write_to_file(f_name, s_channels, f_data, 'w')
@@ -278,6 +289,5 @@ def test(opts):
 
 
 if __name__ == '__main__':
-    opts = docopt.docopt(useStr, version='0.0.2')
-    test(opts)
-
+    OPTS = docopt.docopt(USE_STR, version='0.0.2')
+    test(OPTS)
